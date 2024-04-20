7f01a304978f0ed941b63ef554f96320818a1ae0
trinareyes <katrinajreyes@outlook.com>
2024-04-19 22:23:39 -0700

    updated game
    numbers game changed while loop to for loop

--- src/com/coderscampus/assignment2.java
+++ src/com/coderscampus/assignment2.java
@@ -1,12 +1,42 @@
 package com.coderscampus;
 
-public class assignment2 {
+import java.util.Random;
+import java.util.Scanner;
 
+public class assignment2 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+		int rNumber = random.nextInt(100 + 1);
 
 		
-		//generate random number and collect input
-	}
+		System.out.println("Welcome to the numbers game. Choose a number between 1 and 100!");
 
+		for (int amtGuess = 1; amtGuess < 6; amtGuess++) {
+
+			System.out.println("Choose a number between 1 and 100: ");
+			int guess = scanner.nextInt();
+
+			if (guess == rNumber) {
+				System.out.println("***YOU WIN***");
+				break;
+			} else if (amtGuess == 5) {
+				System.out.println("GAME OVER! The correct number was " + rNumber);
+
+			} else if (guess < 1 || guess > 100) {
+				System.out.println("OOOPs! Please make a guess within range");
+				amtGuess--;
+			} else if (guess > rNumber) {
+				System.out.println("lower!");
+			} else if (guess < rNumber) {
+				System.out.println("higher");
+			} else {
+				break;
+			}
+
+		}
+
+		scanner.close();
+
+	}
 }
